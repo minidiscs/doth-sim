1fad5f67d02a094c503b0215d9b74460ddb72ba2
minidiscs <shanedbutler@gmail.com>
2022-07-07 14:57:42 -0400

    list of businessess added to html

--- scripts/BusinessList.js
+++ scripts/BusinessList.js
@@ -1,3 +1,19 @@
 import { getBusinesses } from "./database.js";
 
 const businesses = getBusinesses()
+
+export const businessList = () => {
+    let businessListHTML = "<h1>Active Businesses</h1>"
+
+    businesses.forEach((business) => {
+        businessListHTML += `
+            <section class="business">
+                <h2 class="business--name">
+                ${business.companyName}</h2>
+                <div class="business--address">
+                ${business.addressFullStreet}<br>
+                ${business.addressCity}, ${business.addressStateCode} ${business.addressZipCode}
+                <hr>`
+    })
+    return businessListHTML    
+}
