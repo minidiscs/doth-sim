54939e718339d8815109e30e7835dd071ca054ae
minidiscs <shanedbutler@gmail.com>
2022-07-09 21:37:36 -0400

    bug fixes

--- scripts/AgentList.js
+++ scripts/AgentList.js
@@ -9,17 +9,19 @@ const purchasingAgents = (businesses) => businesses.map(business => `<h3 class="
 
 export const purchasingAgentsHTML = purchasingAgents(businessesAll).join("")
 
-document.querySelector(".agent--search").addEventListener("input", (keyPressEvent) => {
+document.querySelector(".agent--search").addEventListener("keydown", (keyPressEvent) => {
     const agentSearchResultArticle = document.querySelector(".foundAgent")
-
+    
+    //find object from target.value, set both to upperCase to make case insensitive when calling includes method
     const foundAgent = businessesAll.find(business => business.purchasingAgent.nameLast.toUpperCase().includes(keyPressEvent.target.value.toUpperCase()))
 
     //wrap foundBusiness object in an array to it can be used as an argument to the purchasingAgents function, which expects an array   
     const wrappedFoundAgent = [foundAgent].flat()
-    //add error handling, pass to eachBusiness function
-    if (foundAgent != undefined) {
+    //add error handling, pass to purchasingAgents function when value length > 1
+    if (keyPressEvent.target.value.length > 1) {
         agentSearchResultArticle.innerHTML = purchasingAgents(wrappedFoundAgent)
     }
+    //clear innerHTML when foundAgent doesn't return an agent or value.length is <= 1
     else {
         agentSearchResultArticle.innerHTML = ""
     }
--- scripts/BusinessList.js
+++ scripts/BusinessList.js
@@ -22,14 +22,16 @@ export const businessList = () => eachBusiness(businessesAll)
 document.querySelector(".company--search").addEventListener("input", (keyPressEvent) => {
     const companySearchResultArticle = document.querySelector(".foundBusinesses")
 
+    //find object from target.value, set both to upperCase to make case insensitive when calling includes method
     const foundBusiness = businessesAll.find(business => business.companyName.toUpperCase().includes(keyPressEvent.target.value.toUpperCase()))
 
     //wrap foundBusiness object in an array to it can be used as an argument to the eachBusiness function, which expects an array   
     const wrappedFoundBusiness = [foundBusiness].flat()
-    //add error handling, pass to eachBusiness function
-    if (foundBusiness != undefined) {
+    //add error handling, pass to eachBusiness function when value length >1
+    if (keyPressEvent.target.value.length > 1) {
         companySearchResultArticle.innerHTML = eachBusiness(wrappedFoundBusiness)
     }
+    //clear innerHTML when string <= 1
     else {
         companySearchResultArticle.innerHTML = ""
     }
